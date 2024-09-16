cf941c9f257f203f2d367a47d0049b01acea4c84
Baldeen48 <baldeen@sheridancollege.ca>
2024-09-16 12:49:48 -0400

    Initial commit--added a comment

---
+++ DemonstrationProject.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demonstrationproject;
+
+/**
+ *
+ * @author Owner
+ * @modified by
+ */
+public class DemonstrationProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    //added a comment
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+    }
+    
+}
